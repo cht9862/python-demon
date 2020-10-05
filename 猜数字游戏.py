8e91e544570071874fad9d9f58636ada361804f6
chen <2225995702@qq.com>
2020-10-05 14:49:21 +0800

    v3

--- 猜数字游戏.py
+++ 猜数字游戏.py
@@ -1,36 +1,98 @@
-'''
-game_name：猜数字
-author：Chen Haotian
-目的：
-    系统给出一个两位数的数字，用户通过猜数字输入答案，判断是否对应系统所给出的
-数字，如果对应则通关程序。
+# # def fn():
+# #     '''
+# #     将指定列表中的偶数元素过滤并打印出来
+# #     '''
+# #     print('hello')
+# #     a=[1,2,3,4,5,6,7,8,12]
+# #     def fn1():
+# #         s=[]
+# #         for i in a:
+# #             # print(i)
+# #             if i % 2 == 0:
+# #                 s.append(i)
+# #             # print(s)
+# #         return s
+# #     s=fn1()
+    
+# #     def fn2():
+# #         b=[]
+# #         for i in s:
+# #             if i > 5:
+# #                 b.append(i)
+# #         return b
+    
+# #     a=fn1()
+# #     b=fn2()
+# #     print(b)
+# #     return a
 
-'''
+# # s=fn()
+# # print(s)
 
-print('hello')
 
-print('你好')
+# def fn(a,b):
+#     return a+b
 
-print('再见')
+
+# nu=lambda a,b: a+b
+# print(nu(1,2))
+
+# l=(1,2,3,4,5,6,7,8,9)
+# ne=lambda i: i % 3 == 0 ;print(ne(13.5))
+# fi=filter(lambda i: i%3 ==0,l)
+# print(list(fi))
+# l=[1,2,3,4,5,6,7,8,9]
+# fi=map(lambda i: i*3 ,l)
+# print(l.sort())
+
+
+def number(a,b):
+    '''
+    求任意两个数的和
+    '''
+    return a+b
+
+# def mul(a,b):
+#     '''
+#     求任意两个数的积
+#     '''
+    
+#     return a*b
 
 
 
 def fn():
-    print('这是我的第一个函数')
-    print('今天天气真不错')
-print(type(fn))
-fn()
-#定义一个函数，可以用来求任意两个数的和
-def sumb(a1,a2,argument=3):
-    # Number_a=int(input('请输入第一个数字'))
-    # Number_b=int(input('请输入第二个数字'))
-    # print(argument+Number_b+a)
-    print(a1)
-    print(a2)
-    print(argument)
+    print('hello')
 
-sumb(a2=3,a1=2)
 
-a=[1,2,3,4]
-c=a.copy
-print(id(a),'\n',id(c))
+
+
+def fn2():
+    print('开始打印')
+    fn()
+    print('打印结束')
+
+def bend(a):
+    def new_function(*ole,**keyole):
+        print('开始打印')
+        r = a(*ole,**keyole)
+        print('打印结束')
+        return r
+    return new_function
+# @fn2
+@bend
+def number(a,b):
+    '''
+    求任意两个数的和
+    '''
+    return a+b
+
+def sya_hello():
+    print('大家好')    
+
+chen=number(123,456)
+print(chen)
+# f1=bend(number)
+# f2=bend(number)
+# r=f1(123,456)
+# print(r)
